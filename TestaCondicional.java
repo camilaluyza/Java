8aa16805e8bfda860df99656bb6153a32f45f3fc
Camila Luiza <78321076+camilaluyza@users.noreply.github.com>
2022-08-04 18:22:27 -0300

    Update TestaCondicional.java
     comentário condição &&

--- TestaCondicional.java
+++ TestaCondicional.java
@@ -6,7 +6,7 @@ public class TestaCondicional {
 		int idade = 16;
 		int quantidadePessoas = 3;
 		
-		if(idade >= 18) { // Pode simplificar fazendo dessa forma assim reduz o codigo if(idade >= 18 || quantidadePessoas >=2)
+		if(idade >= 18) { // Pode simplificar fazendo dessa forma assim reduz o codigo  if(idade >= 18 || quantidadePessoas >=2) ou $$ condição, exemplo : você não pode entrar sozinho
 			System.out.println("Você tem mais de 18 anos");
 			System.out.println("Seja bem vindo");
 		}
