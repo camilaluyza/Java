e5f8f01766a5e9305047ae077eeeec3f9babbec5
Camila Luiza <78321076+camilaluyza@users.noreply.github.com>
2022-08-08 16:13:32 -0300

    Add files via upload

---
+++ MultiplosDeTresAteCem.java
@@ -0,0 +1,20 @@
+
+public class MultiplosDeTresAteCem {
+	    public static void main (String[] args) {
+	        for (int i = 1; i < 100; i++ ){
+	            if (i % 3 == 0)    {
+	                System.out.println(i);
+	            }
+	        }
+	    }
+	}
+
+
+//entre outras tantas opções, a mais simples:
+//class MultiplosDeTresAteCem {
+//public static void main (String[] args) {
+ //   for (int i = 3; i < 100; i += 3 ){
+   //     System.out.println(i);
+    //}
+//}
+//}
